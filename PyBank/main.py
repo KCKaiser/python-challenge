215516cd38a021c9d3245a6a41ecf849cf5fe694
KCKaiser <kc7mt@virginia.edu>
2018-06-11 17:11:28 -0400

    Python Homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,61 +1,63 @@
-import os, csv
+import glob, os, csv
 
-pybankcsvpath = os.path.join('raw_data','budget_data_2.csv')
+# LOOPS THROUGH ALL THE .CSV FILES IN raw_data FOLDER
+for filename in glob.glob('./raw_data/*.csv'):
+    date = []
+    rev = []
+    with open(filename, newline = "") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter = ',')
+        for row in csvreader:
+            # Creating list of Dates
+            date.append(row[0])
+            # Creating list of Revenue
+            rev.append(row[1])
+        # Getting rid of headers
+        date = date[1:]
+        rev = rev[1:]
 
-date=[]
-rev = []
-with open(pybankcsvpath, newline = "") as csvfile:
-    print(csvfile)
-    csvreader = csv.reader(csvfile, delimiter = ',')
-
-    for row in csvreader:
-        date_start = row[0]
-        rev_start = row[1]
-        date.append(row[0])
-        rev.append(row[1])
-    date = date[1:]
-    rev = rev[1:]
-
+    # Convert Revnue into integers
     rev_num = [int(num) for num in rev]
 
+    # Revenue Max
     rev_max = max(rev_num)
-    rev_min = min(rev_num)
-    
-    min_index =rev_num.index(rev_min)
-    max_index =rev_num.index(rev_max)
+    max_index = rev_num.index(rev_max)
+    # Date of Max Revanue
+    date_max = date[max_index] 
 
+    # Revnue Min
+    rev_min = min(rev_num) 
+    min_index = rev_num.index(rev_min)
+    # Date of Min Revenue
     date_min = date[min_index]
-    date_max = date[max_index]
 
+    # Total Months
     num_months = len(date)
 
+    # Total Revenue
     rev_sum = sum(rev_num)
-    rev_avg = float(sum(rev_num))/len(rev_num)
-    print(rev_avg)
+    # Average Revenue
+    rev_avg = rev_sum/len(rev_num)
 
-    print("```")
-    print("Financial Analysis")
-    print("----------------------------")
-    print("Total Months: " + str(num_months))
-    print("Total Revenue: $" + str(rev_sum))
-    print("Average Revenue Change: $" + str(rev_avg))
-    print(f'Greatest Increase in Revenue: {date_max} (${rev_max})')
-    print(f'Greatest Decrease in Revenue: {date_min} (${rev_min})')
-    print("```")
-
-    filename = 'budget_data_2.txt'
-    with open(filename,'w') as f:
+    # Create text file
+    with open("." + filename[10:-4] + '.txt', 'w') as f:
         f.write("```\n")
-        f.write("Financial Analysis\n")
+        f.write("Financial Analysis (" + filename[11:-4] + ")\n")
         f.write("----------------------------\n")
         f.write("Total Months: " + str(num_months)+"\n")
         f.write("Total Revenue: $" + str(rev_sum)+"\n")
-        f.write("Average Revenue Change: $" + str(rev_avg)+"\n")
+        f.write('Average Revenue Change: ' + "$" + "{:.2f}".format(rev_avg) + "\n")
         f.write(f'Greatest Increase in Revenue: {date_max} (${rev_max})\n')
         f.write(f'Greatest Decrease in Revenue: {date_min} (${rev_min})\n')
         f.write("```")
 
+    # Print
+    print("```")
+    print("Financial Analysis (" + filename[11:-4] + ")\n")
+    print("----------------------------")
+    print("Total Months: " + str(num_months))
+    print("Total Revenue: $" + str(rev_sum))
+    print('Average Revenue Change: ' + "$" + "{:.2f}".format(rev_avg))
+    print(f'Greatest Increase in Revenue: {date_max} (${rev_max})')
+    print(f'Greatest Decrease in Revenue: {date_min} (${rev_min})')
+    print("```")
 
-
-
-        
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,32 +1,69 @@
-import os, csv
+import os, csv, glob, collections
 
-pypollcsvpath = os.path.join('raw_data','election_data_1.csv')
+# LOOP THROUGH ALL THE .CSV FILES IN raw_data FOLDER
+for filename in glob.glob('./raw_data/*.csv'):
 
-identification=[]
-county = []
-names = []
-with open(pypollcsvpath, newline = "") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
-
-    for row in csvreader:
-        identification_start = row[0]
-        county_start = row[1]
-        names_start = row[2]
-
-        identification.append(row[0])
-        county.append(row[1])
-        names.append(row[2])
+    identification=[]
+    names = []
+    with open(filename, newline = "") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter = ',')
+        for row in csvreader:
+            # Creating list of IDs
+            identification.append(row[0])
+            # Creating list of Candidates voted
+            names.append(row[2])
+        # Getting rid of headers
+        identification = identification[1:]
+        names = names[1:]
     
-    identification = identification[1:]
-    county = county[1:]
-    names = names[1:]
-
+    # Number of votes
     num_votes = len(identification)
-    print(num_votes)
-    names_set = set(names)
-    names_ls=list(names_set)
-    names_sort= (sorted(names_ls))
-    print(names_sort[0])
 
-    
-    
+    # Converting names to set format to find unique candidates
+    candidates_set = set(names)
+    # Converting the set back to list format
+    candidates_ls = list(candidates_set)
+    # Sort list of candidates alphabetically
+    candidate_list= (sorted(candidates_ls))
+
+    # Sort the full list of candidates voted alphabetically
+    full_names_sort= (sorted(names))
+
+    # Go through full list of candidates names to count votes and calculate percentage
+    count = {}
+    percent = {}
+    for x in candidate_list:
+        counter = full_names_sort.count(x)
+        count[x]= counter
+        percent[x] = (count[x]/num_votes)
+
+    # Determine which candidates received highest number of votes
+    win = collections.Counter(count)
+    winning = win.most_common(1)
+
+    # Write text file
+    with open('.' + filename[10:-4] + '.txt', 'w') as f:
+        f.write('```\n')
+        f.write('Election Results (' + filename[11:-4] + ')\n')
+        f.write('-------------------------\n')
+        f.write('Total Votes: ' + str(num_votes) + "\n")
+        f.write('-------------------------\n')
+        for x in candidate_list:
+            f.write(f'{x}:  {percent[x]:.1%} ({count[x]}) \n')
+        f.write('-------------------------\n')
+        f.write('Winner: ' + winning[0][0] +'\n')
+        f.write('-------------------------\n')
+        f.write('```')
+
+    # Print
+    print('```')
+    print('Election Results (' + filename[11:-4] + ")")
+    print('-------------------------')
+    print('Total Votes: ' + str(num_votes))
+    print('-------------------------')
+    for x in candidate_list:
+        print(f'{x}:  {percent[x]:.1%} ({count[x]})')
+    print('-------------------------')
+    print('Winner: ' + winning[0][0])
+    print('-------------------------')
+    print('```')
